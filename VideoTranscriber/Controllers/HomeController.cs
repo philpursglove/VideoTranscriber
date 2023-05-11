6180ad4bead5e48bb90b9b3a617c5a1638b2e28b
Phil Pursglove <philpursglove@users.noreply.github.com>
2023-05-11 12:08:05 +0100

    She works, she works!

--- VideoTranscriber/Controllers/HomeController.cs
+++ VideoTranscriber/Controllers/HomeController.cs
@@ -1,16 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Web;
 using Azure.Storage.Blobs;
 using VideoTranscriber.Models;
-using Azure.Core;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Text.Encodings.Web;
-using System.Text.Json.Serialization;
-using System.Text.Json;
-using Azure.Identity;
-using System.Web;
-using Azure.Storage.Blobs.Models;
+using Newtonsoft.Json;
 
 namespace VideoTranscriber.Controllers
 {
@@ -18,33 +11,20 @@ namespace VideoTranscriber.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly string _connString;
-        private readonly string _subscriptionId;
-        private readonly string _resourceGroup;
-        private readonly string _accountName;
+        private readonly string _accountId;
+        private readonly string _apiKey;
 
         public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
             _connString = configuration.GetConnectionString("VideoTranscriberStorageAccount");
-            _subscriptionId = configuration["SubscriptionId"];
-            _resourceGroup = configuration["ResourceGroup"];
-            _accountName = configuration["AccountName"];
+            _accountId = configuration["AccountId"];
+            _apiKey = configuration["ApiKey"];
         }
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
         }
 
         [HttpPost, ValidateAntiForgeryToken]
@@ -59,225 +39,102 @@ namespace VideoTranscriber.Controllers
 
                 string videoUrl = blobClient.Uri.ToString();
 
-                IndexVideo(videoUrl).Wait();
+                IndexVideo(videoUrl, model.VideoFile.FileName).Wait();
             }
 
             return RedirectToAction(nameof(Index));
         }
 
-        private async Task IndexVideo(string videoUrl)
+        private async Task IndexVideo(string videoUrl, string videoName)
         {
-            var videoIndexerResourceProviderClient = new VideoIndexerResourceProviderClient("https://management.azure.com", _subscriptionId, _accountName.Replace(" ", "%20"));
-
-            // Get account details
-            var account = await videoIndexerResourceProviderClient.GetAccount();
-            var accountId = account.Properties.Id;
-            var accountLocation = account.Location;
-
-            // Get account-level access token for Azure Video Indexer
-            var accessTokenRequest = new AccessTokenRequest
-            {
-                PermissionType = AccessTokenPermission.Contributor,
-                Scope = ArmAccessTokenScope.Account
-            };
-
-            var accessToken = await videoIndexerResourceProviderClient.GetAccessToken(accessTokenRequest);
             var apiUrl = "https://api.videoindexer.ai";
+            var location = "trial"; // replace with the account's location, or with “trial” if this is a trial account
+
             System.Net.ServicePointManager.SecurityProtocol = System.Net.ServicePointManager.SecurityProtocol | System.Net.SecurityProtocolType.Tls12;
 
+            // create the http client
             var handler = new HttpClientHandler();
             handler.AllowAutoRedirect = false;
             var client = new HttpClient(handler);
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _apiKey);
 
-            MultipartFormDataContent content = null;
-            var queryParams = CreateQueryString(
-                new Dictionary<string, string>()
-                {
-                    {"accessToken", accessToken},
-                    {"name", "video sample"},
-                    {"description", "video_description"},
-                    {"privacy", "private"},
-                    {"partition", "partition"},
-                    {"videoUrl", videoUrl},
-                });
-            var uploadRequestResult = await client.PostAsync($"{apiUrl}/{accountLocation}/Accounts/{accountId}/Videos?{queryParams}", content);
-            var uploadResult = await uploadRequestResult.Content.ReadAsStringAsync();
+            // obtain account access token
+            var accountAccessTokenRequestResult = client.GetAsync($"{apiUrl}/auth/{location}/Accounts/{_accountId}/AccessToken?allowEdit=true").Result;
+            var accountAccessToken = accountAccessTokenRequestResult.Content.ReadAsStringAsync().Result.Replace("\"", "");
 
-            // Get the video ID from the upload result
-            string videoId = JsonSerializer.Deserialize<Video>(uploadResult).Id;
+            client.DefaultRequestHeaders.Remove("Ocp-Apim-Subscription-Key");
 
+            // upload a video
+            var content = new MultipartFormDataContent();
+            //Debug.WriteLine("Uploading...");
+            // get the video from URL
+            //var videoUrl = "VIDEO_URL"; // replace with the video URL
+
+            // as an alternative to specifying video URL, you can upload a file.
+            // remove the videoUrl parameter from the query string below and add the following lines:
+            //FileStream video =File.OpenRead(Globals.VIDEOFILE_PATH);
+            //byte[] buffer = new byte[video.Length];
+            //video.Read(buffer, 0, buffer.Length);
+            //content.Add(new ByteArrayContent(buffer));
+
+            string correctedName = videoName.Replace(" ", string.Empty);
+            if (correctedName.Length > 80)
+            {
+                correctedName = correctedName.Substring(0, 80);
+            }
+
+            var uploadRequestResult = client.PostAsync($"{apiUrl}/{location}/Accounts/{_accountId}/Videos?accessToken={accountAccessToken}&name={correctedName}&description=some_description&privacy=private&partition=some_partition&videoUrl={videoUrl}&indexingPreset=BasicAudio", content).Result;
+            var uploadResult = uploadRequestResult.Content.ReadAsStringAsync().Result;
+
+            // get the video id from the upload result
+            var videoId = JsonConvert.DeserializeObject<dynamic>(uploadResult)["id"];
+
+            // obtain video access token
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _apiKey);
+            var videoTokenRequestResult = client.GetAsync($"{apiUrl}/auth/{location}/Accounts/{_accountId}/Videos/{videoId}/AccessToken?allowEdit=true").Result;
+            var videoAccessToken = videoTokenRequestResult.Content.ReadAsStringAsync().Result.Replace("\"", "");
+
+            client.DefaultRequestHeaders.Remove("Ocp-Apim-Subscription-Key");
+
+            // wait for the video index to finish
             while (true)
             {
-                await Task.Delay(10000);
+                Thread.Sleep(10000);
 
-                queryParams = CreateQueryString(
-                    new Dictionary<string, string>()
-                    {
-                        {"accessToken", accessToken},
-                        {"language", "English"},
-                    });
+                var videoGetIndexRequestResult = client.GetAsync($"{apiUrl}/{location}/Accounts/{_accountId}/Videos/{videoId}/Index?accessToken={videoAccessToken}&language=English").Result;
+                var videoGetIndexResult = videoGetIndexRequestResult.Content.ReadAsStringAsync().Result;
 
-                var videoGetIndexRequestResult = await client.GetAsync($"{apiUrl}/{accountLocation}/Accounts/{accountId}/Videos/{videoId}/Index?{queryParams}");
-                var videoGetIndexResult = await videoGetIndexRequestResult.Content.ReadAsStringAsync();
+                var processingState = JsonConvert.DeserializeObject<dynamic>(videoGetIndexResult)["state"];
 
-                string processingState = JsonSerializer.Deserialize<Video>(videoGetIndexResult).State;
+                //Debug.WriteLine("");
+                //Debug.WriteLine("State:");
+                //Debug.WriteLine(processingState);
 
-                ;
-
-                // Job is finished
+                // job is finished
                 if (processingState != "Uploaded" && processingState != "Processing")
                 {
-                    
+                    //Debug.WriteLine("");
+                   // Debug.WriteLine("Full JSON:");
+                    //Debug.WriteLine(videoGetIndexResult);
+                    var result = JsonConvert.DeserializeObject<dynamic>(videoGetIndexResult);
+
+                    var video = result.videos[0];
+                    var insights = video.insights;
+                    string language = insights.sourceLanguage;
+                    var transcript = insights.transcript;
+
+                    string assembledTranscript = "";
+                    foreach (var transcriptItem in transcript)
+                    {
+                        assembledTranscript += transcriptItem.text;
+                    }
+
                     break;
                 }
             }
 
-            queryParams = CreateQueryString(
-                new Dictionary<string, string>()
-                {
-                    {"accessToken", accessToken},
-                    {"id", videoId},
-                });
-
-            var searchRequestResult = await client.GetAsync($"{apiUrl}/{accountLocation}/Accounts/{accountId}/Videos/Search?{queryParams}");
-            var searchResult = await searchRequestResult.Content.ReadAsStringAsync();
-
-
-        }
-
-        static string CreateQueryString(IDictionary<string, string> parameters)
-        {
-            var queryParameters = HttpUtility.ParseQueryString(string.Empty);
-            foreach (var parameter in parameters)
-            {
-                queryParameters[parameter.Key] = parameter.Value;
-            }
-
-            return queryParameters.ToString();
         }
     }
 }
 
-public class VideoIndexerResourceProviderClient
-{
-    private readonly string armAaccessToken;
-    private readonly string _AzureResourceManager = "https://management.azure.com";
-    private readonly string _SubscriptionId = ""; // Your Azure subscription
-    private readonly string _ResourceGroup = ""; // Your resource group
-    private readonly string _AccountName = "";
-
-    public VideoIndexerResourceProviderClient(string azureResourceManager, string subscriptionId, string accountName)
-    {
-        _AzureResourceManager = azureResourceManager;
-        _SubscriptionId = subscriptionId;
-        _AccountName = accountName;
-    }
-
-    async public Task<VideoIndexerResourceProviderClient> BuildVideoIndexerResourceProviderClient()
-    {
-        var tokenRequestContext = new TokenRequestContext(new[] { $"{_AzureResourceManager}/.default" });
-        var tokenRequestResult = await new DefaultAzureCredential().GetTokenAsync(tokenRequestContext);
-        return new VideoIndexerResourceProviderClient(tokenRequestResult.Token);
-    }
-    public VideoIndexerResourceProviderClient(string armAaccessToken)
-    {
-        this.armAaccessToken = armAaccessToken;
-    }
-
-    public async Task<string> GetAccessToken(AccessTokenRequest accessTokenRequest)
-    {
-        Console.WriteLine($"Getting access token. {JsonSerializer.Serialize(accessTokenRequest)}");
-        // Set the generateAccessToken (from video indexer) HTTP request content
-        var jsonRequestBody = JsonSerializer.Serialize(accessTokenRequest);
-        var httpContent = new StringContent(jsonRequestBody, System.Text.Encoding.UTF8, "application/json");
-
-        // Set request URI
-        var requestUri = $"{_AzureResourceManager}/subscriptions/{_SubscriptionId}/resourcegroups/{_ResourceGroup}/providers/Microsoft.VideoIndexer/accounts/{_AccountName}/generateAccessToken?api-version=2021-08-16-preview";
-
-        // Generate access token from video indexer
-        var client = new HttpClient(new HttpClientHandler());
-        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", armAaccessToken);
-        var result = await client.PostAsync(requestUri, httpContent);
-        var jsonResponseBody = await result.Content.ReadAsStringAsync();
-        return JsonSerializer.Deserialize<GenerateAccessTokenResponse>(jsonResponseBody).AccessToken;
-    }
-
-    public async Task<Account> GetAccount()
-    {
-
-        Console.WriteLine($"Getting account.");
-        // Set request URI
-        var requestUri = $"{_AzureResourceManager}/subscriptions/{_SubscriptionId}/resourcegroups/{_ResourceGroup}/providers/Microsoft.VideoIndexer/accounts/{_AccountName}/?api-version=2021-08-16-preview";
-
-        // Get account
-        var client = new HttpClient(new HttpClientHandler());
-        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", armAaccessToken);
-        var result = await client.GetAsync(requestUri);
-        var jsonResponseBody = await result.Content.ReadAsStringAsync();
-        return JsonSerializer.Deserialize<Account>(jsonResponseBody);
-    }
-}
-
-public class AccessTokenRequest
-{
-    [JsonPropertyName("permissionType")]
-    public AccessTokenPermission PermissionType { get; set; }
-
-    [JsonPropertyName("scope")]
-    public ArmAccessTokenScope Scope { get; set; }
-
-    [JsonPropertyName("projectId")]
-    public string ProjectId { get; set; }
-
-    [JsonPropertyName("videoId")]
-    public string VideoId { get; set; }
-}
-
-[JsonConverter(typeof(JsonStringEnumConverter))]
-public enum AccessTokenPermission
-{
-    Reader,
-    Contributor,
-    MyAccessAdministrator,
-    Owner,
-}
-
-[JsonConverter(typeof(JsonStringEnumConverter))]
-public enum ArmAccessTokenScope
-{
-    Account,
-    Project,
-    Video
-}
-
-public class GenerateAccessTokenResponse
-{
-    [JsonPropertyName("accessToken")]
-    public string AccessToken { get; set; }
-
-}
-public class AccountProperties
-{
-    [JsonPropertyName("accountId")]
-    public string Id { get; set; }
-}
-
-public class Account
-{
-    [JsonPropertyName("properties")]
-    public AccountProperties Properties { get; set; }
-
-    [JsonPropertyName("location")]
-    public string Location { get; set; }
-
-}
-
-public class Video
-{
-    [JsonPropertyName("id")]
-    public string Id { get; set; }
-
-    [JsonPropertyName("state")]
-    public string State { get; set; }
-}
     
