01ae0a260fef57e54b76b0269a6b1b8b630a4924
Phil Pursglove <philpursglove@users.noreply.github.com>
2023-06-10 22:44:55 +0100

    Add Arm client

---
+++ VideoTranscriberVideoClient/VideoIndexerClientArm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoTranscriberVideoClient
+{
+    public class VideoIndexerClientArm : VideoIndexerClientBase, IVideoIndexerClient
+    {
+        public Task<IndexingResult> GetVideoIndex(string videoIndexerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IndexingResult> IndexVideo(Uri videoUrl, string videoName, Guid videoGuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SubmitVideoForIndexing(Uri videoUri, string videoName, Guid videoId, Uri callbackUri)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
