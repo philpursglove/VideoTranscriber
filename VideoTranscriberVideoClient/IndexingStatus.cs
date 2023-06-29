14e35ee60eadc77321b887480693880d73d38861
Phil Pursglove <philpursglove@users.noreply.github.com>
2023-06-29 23:38:01 +0100

    Add Processed

--- VideoTranscriberVideoClient/IndexingStatus.cs
+++ VideoTranscriberVideoClient/IndexingStatus.cs
@@ -5,4 +5,5 @@ internal static class IndexingStatus
     internal const string Uploaded = "Uploaded";
     internal const string Processing = "Processing";
     internal const string Failed = "Failed";
+    internal const string Processed = "Processed";
 }
