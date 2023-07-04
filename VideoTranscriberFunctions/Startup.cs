ebdfb78b5c8aa0bbda5746e22964f07f609b7600
Phil Pursglove <philpursglove@users.noreply.github.com>
2023-07-04 22:49:53 +0100

    Add custom ConfigureAppCOnfiguration

--- VideoTranscriberFunctions/Startup.cs
+++ VideoTranscriberFunctions/Startup.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
 
 [assembly: FunctionsStartup(typeof(VideoTranscriberFunctions.Startup))]
 
@@ -8,5 +12,20 @@ public class Startup : FunctionsStartup
 {
     public override void Configure(IFunctionsHostBuilder builder)
     {
+        var configuration = builder.Services.BuildServiceProvider().GetService<IConfiguration>();
+
+
+    }
+
+    public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+    {
+        FunctionsHostBuilderContext context = builder.GetContext();
+
+        builder.ConfigurationBuilder
+            .SetBasePath(context.ApplicationRootPath)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+            .AddJsonFile($"appsettings.{context.EnvironmentName}.json", optional: true, reloadOnChange: false)
+            .AddUserSecrets(Assembly.GetExecutingAssembly(), true, true)
+            .AddEnvironmentVariables();
     }
 }
