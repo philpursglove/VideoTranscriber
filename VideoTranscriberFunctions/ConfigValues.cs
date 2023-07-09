45ac81008e967034d05a859aaafe154734ed0f68
Phil Pursglove <philpursglove@users.noreply.github.com>
2023-07-09 23:05:41 +0100

    Fix visibility

--- VideoTranscriberFunctions/ConfigValues.cs
+++ VideoTranscriberFunctions/ConfigValues.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace VideoTranscriberFunctions
 {
-    internal class ConfigValues
+    public class ConfigValues
     {
         public Uri CallbackUrl { get; set; }
     }
