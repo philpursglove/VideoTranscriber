c2ab66df46524b3ea97b6b9c02c6981b82837af5
Phil Pursglove <philpursglove@users.noreply.github.com>
2023-08-27 23:23:12 +0100

    Second function tests

---
+++ VideoTranscriberTests/IndexCompleteCallbackTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Primitives;
+using NSubstitute;
+using NUnit.Framework;
+using VideoTranscriberData;
+using VideoTranscriberFunctions;
+using VideoTranscriberStorage;
+using VideoTranscriberVideoClient;
+
+namespace VideoTranscriberTests;
+
+[TestFixture]
+public class IndexCompleteCallbackTests
+{
+    private IVideoIndexerClient _videoIndexerClient;
+    private ITranscriptionDataRepository _repository;
+    private IStorageClient _storageClient;
+
+    private IndexCompleteCallback _indexCompleteCallback;
+
+    [SetUp]
+    public void Setup()
+    {
+        _videoIndexerClient = Substitute.For<IVideoIndexerClient>();
+        _repository = Substitute.For<ITranscriptionDataRepository>();
+        _storageClient = Substitute.For<IStorageClient>();
+
+        _indexCompleteCallback = new IndexCompleteCallback(_videoIndexerClient, _repository, _storageClient);
+    }
+
+    [Test]
+    public async Task WhenVideoIsNotProcessedNothingElseHappens()
+    {
+        HttpRequest request = Substitute.For<HttpRequest>();
+        request.Query.Returns(new QueryCollection(new Dictionary<string, StringValues>
+        {
+            {"id", "1234"},
+            {"state", "processing"}
+        }));
+
+        await _indexCompleteCallback.Run(request, null);
+
+        await _videoIndexerClient.DidNotReceive().GetVideoIndex(Arg.Any<string>());
+    }
+}
