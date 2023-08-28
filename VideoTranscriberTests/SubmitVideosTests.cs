287187857b61f30160278a1c52c0ac1dd37ab7c5
Phil Pursglove <philpursglove@users.noreply.github.com>
2023-08-28 22:42:49 +0100

    Test around submission process

--- VideoTranscriberTests/SubmitVideosTests.cs
+++ VideoTranscriberTests/SubmitVideosTests.cs
@@ -1,4 +1,5 @@
 using NSubstitute;
+using VideoTranscriberCore;
 using VideoTranscriberData;
 using VideoTranscriberFunctions;
 using VideoTranscriberStorage;
@@ -33,6 +34,17 @@ namespace VideoTranscriberTests.Functions
             await _submitVideos.Run(null, null);
 
             _videoIndexerClient.DidNotReceive().SubmitVideoForIndexing(Arg.Any<Uri>(), Arg.Any<string>(), Arg.Any<Guid>(), Arg.Any<System.Uri>());
+        }
+
+        [Test]
+        public async Task WhenOneFileIsPresentItIsMovedToTheProcessingFolder()
+        {
+            _storageClient.GetFileNames("toBeProcessed").Returns(new List<string> { "toBeProcessed/fileName.mp4" });
+            _repository.Get("fileName.mp4").Returns(new TranscriptionData { id = Guid.NewGuid() });
+
+            await _submitVideos.Run(null, null);
+
+            await _storageClient.Received().MoveToFolder("toBeProcessed/fileName.mp4", "processing");
         }
     }
 }
